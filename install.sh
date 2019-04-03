52e785e8127ae0702bb12ec423e9e6fb494ee543
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2019-04-03 15:12:17 -0300

    Add script main (install) e alocacao dos demais para diretorio 'scripts'

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+### NOME: Preparar Meu Ambiente De Desenvolvimento No Linux Mint
+### VERSAO: 1.0
+### DATA: 03/04/2019
+### AUTOR: Leandro da Silva Alves
+### DISTRO: Linux Mint 19
+
+echo  INSTALACAO DOS PROGRAMAS ESTA SENDO INICIALIZADA
+
+./scripts/1-install-curl.sh 
+./scripts/2-install-chrome.sh 
+./scripts/3-install-docker.sh 
+./scripts/4-install-git.sh 
+./scripts/5-install-vscode.sh 
+./scripts/6-install-node-npm.sh 
+./scripts/7-install-angular-cli.sh
+
+echo A INSTALACAO DE TODOS OS PROGRAMAS FOI REALIZADA COM SUCESSO!
