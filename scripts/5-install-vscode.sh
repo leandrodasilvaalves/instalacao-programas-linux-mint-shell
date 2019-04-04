e9e2fea2a8905d00efb962a9577f71bf9d26370d
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2019-04-04 14:35:28 -0300

    Ajustes no install-vscode

--- scripts/5-install-vscode.sh
+++ scripts/5-install-vscode.sh
@@ -4,7 +4,7 @@ wget "https://go.microsoft.com/fwlink/?LinkID=620884" -O vscode.tar.gz
 sudo tar -vzxf vscode.tar.gz -C /opt/
 sudo mv /opt/VSCode*/ /opt/vscode/
 sudo ln -sf /opt/vscode/code /usr/bin/code
-echo -e '[Desktop Entry]\n Version=1.0\n Name=vscode\n Exec=/opt/vscode/VSCode-linux-x64/code\n Icon=/opt/vscode/VSCode-linux-x64/resources/app/resources/linux/code.png\n Type=Application\n Categories=Application' | sudo tee /usr/share/applications/vscode.desktop
+echo -e '[Desktop Entry]\n Version=1.0\n Name=vscode\n Exec=/opt/vscode/code\n Icon=/opt/vscode/resources/app/resources/linux/code.png\n Type=Application\n Categories=Application' | sudo tee /usr/share/applications/vscode.desktop
 rm vscode.tar.gz
 
 echo FINALIZANDO A ISNTALACAO DO VSCODE
