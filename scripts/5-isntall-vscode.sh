5bfcf698a25fc88d0b2fd290a5ddaf535301139d
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2019-04-04 16:15:42 -0300

    Ajustes install vscode

---
+++ scripts/5-isntall-vscode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo \nINICIANDO A INSTALACAO DO VSCODE
+
+wget -C https://az764295.vo.msecnd.net/stable/0dd516dd412d42323fc3464531b1c715d51c4c1a/code_1.33.0-1554390824_amd64.deb
+sudo dpkg -i code_1.33.0-1554390824_amd64.deb
+sudo apt-get install -f
+rm code_1.33.0-1554390824_amd64.deb
+
+echo \nFINALIZANDO A INSTALACAO DO VSCODE
+printf '=%.0s' {1..100}
