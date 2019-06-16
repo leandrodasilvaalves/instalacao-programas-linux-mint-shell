d1d15fc65bbaba8443527b4b0fa60be87e5547ad
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2019-06-16 09:24:56 -0300

    Add config git e ajuste url spotfy

--- scripts/4-install-git.sh
+++ scripts/4-install-git.sh
@@ -2,7 +2,13 @@
 
 echo INICIANDO A ISNTALACAO DO GIT
 
+
 sudo apt-get install -y git
+git --version
+git config --global user.name 'Leandro da Silva Alves'
+git config --global user.email  leandro.silva.alves86@gmail.com
+ssh-keygen -t rsa -b 4096 -C leandro.silva.alves86@gmail.com
+
 
 echo FINALIZANDO A INSTALACAO DO GIT
 printf '=%.0s' {1..100}
